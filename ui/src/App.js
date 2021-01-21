f05f0c54eaf244658cc28ca55c3807aa5a288d78
Kyle Krupp <kyle.krupp@gmail.com>
2021-01-21 15:45:02 -0700

    only show login prompt for mobile testing

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,23 +2,16 @@ import "antd/dist/antd.css"
 import "./index.css"
 import { Row, Col } from 'antd'
 import { CardContainer } from './app/components/card'
-import { GiftForm } from './app/components/form'
 import { LoginCard } from "./app/components/login"
-import { useState } from 'react'
 
 
 const App = () =>  {
-
-  const [isLoggedIn, setLoggedIn] = useState(false)
-
-  const handleLogin = () => setLoggedIn(true)
-
   return (
     <>
-        <Row className={"container"}>
+    <Row className={"container"}>
         <CardContainer>
       <Col span={24}>
-    {isLoggedIn ? <GiftForm/> : <LoginCard/>}
+        <LoginCard/>
       </Col>
       </CardContainer>
       </Row>
