601f6a0259627c2a5ea8667e3e6889557657180e
Kyle Krupp <kyle.krupp@gmail.com>
2021-02-07 09:38:21 -0700

    minor form updates

--- ui/src/app/components/login/index.tsx
+++ ui/src/app/components/login/index.tsx
@@ -5,7 +5,7 @@ export const LoginCard = () => {
   const connectWithHandCash = () => window.location.href = 'https://app.handcash.io/#/authorizeApp?appId=5fff949f4033300c3d87aed1'
   return (
     <>
-        <Image src={'https://bitcoin-sv-gifter.s3.amazonaws.com/mitto-logo-resizedv1.png'} className={'logo-image'}width={100}></Image>
+        <Image src={'https://bitcoin-sv-gifter.s3.amazonaws.com/mitto-logo-dark.png'} className={'logo-image'}width={100}></Image>
 
         <div onClick={connectWithHandCash} id="connectButton" app-id="123-456">Connect with HandCash</div>
     </>
