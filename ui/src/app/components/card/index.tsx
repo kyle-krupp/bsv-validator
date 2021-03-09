a14127ee97e6afc95bed068732ec722600f5159f
Kyle Krupp <kyle.krupp@gmail.com>
2021-03-08 20:17:35 -0700

    move react app out of ui directory

--- ui/src/app/components/card/index.tsx
+++
@@ -1,4 +0,0 @@
-import React, { PropsWithChildren } from 'react'
-import { Card } from 'antd'
-
-export const CardContainer = (props: PropsWithChildren<any>): React.ReactElement => <Card className={'card'} bordered={false}>{props.children}</Card>
