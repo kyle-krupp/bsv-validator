10fe582973ab772bb1c9c5a9c666a88eb9427033
Kyle Krupp <kyle.krupp@gmail.com>
2021-03-10 18:48:29 -0700

    add index to send result path import

--- src/app/components/form/index.jsx
+++ src/app/components/form/index.jsx
@@ -3,7 +3,7 @@ import { Form, Input, Button, Avatar, Row, Col, Tooltip } from 'antd';
 import { CardContainer } from '../card';
 import { useHistory } from 'react-router-dom'
 import getSymbolFromCurrency from 'currency-symbol-map'
-import { SendResult } from './sendResult';
+import { SendResult } from './sendResult/index';
 
 export const GiftForm = () => {
   let history = useHistory()
